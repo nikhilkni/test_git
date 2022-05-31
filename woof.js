d6093892f2984b805567f117daf2d38085d167df
nikhilkni <nikhil.shakya2010@gmail.com>
2022-05-31 15:12:30 +0530

    testing_file

---
+++ woof.js
@@ -0,0 +1,6 @@
+function woof(str){
+    console.log('The woof ran: ',str);
+    return str = 'woof!';
+}
+
+module.exports = woof;
---
+++ woof.test.js
@@ -0,0 +1,5 @@
+const woof = require('./woof');
+test ('Our first test',function(){
+    woof("oh hero");
+});
+test('failing above',()=>{});
